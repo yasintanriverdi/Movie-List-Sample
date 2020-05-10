23d2ea82876377af464cb399ef06798164299aa8
yasintanriverdi <yasin.tanriverdi@zenjob.com>
2020-05-10 12:04:39 +0200

    buildSrc is added with initial dependencies

---
+++ buildSrc/src/main/java/com/yasintanriverdi/movielistsample/buildsrc/Dependencies.kt
@@ -0,0 +1,55 @@
+object Dependencies {
+    object Kotlin {
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:${Versions.kotlin}"
+    }
+
+    object AndroidX {
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+        const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+
+        object Navigation {
+            const val fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+            const val ui = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+            const val safeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
+        }
+
+        object Fragment {
+            const val fragment = "androidx.fragment:fragment:${Versions.fragment}"
+            const val fragmentKtx = "androidx.fragment:fragment-ktx:${Versions.fragment}"
+        }
+
+        object Test {
+            const val core = "androidx.test:core:${Versions.test}"
+            const val runner = "androidx.test:runner:${Versions.test}"
+            const val rules = "androidx.test:rules:${Versions.test}"
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+        }
+
+        const val constraintlayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+
+        const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+
+        object Lifecycle {
+            const val extensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
+            const val viewmodelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+        }
+    }
+
+    object Dagger {
+        const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+        const val compiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+    }
+
+    object Retrofit {
+        const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+        const val gsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    }
+
+    object OkHttp {
+        const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}"
+    }
+}
---
+++ buildSrc/src/main/java/com/yasintanriverdi/movielistsample/buildsrc/Versions.kt
@@ -0,0 +1,18 @@
+object Versions {
+    const val kotlin = "1.3.72"
+    const val dagger = "2.27"
+    const val retrofit = "2.8.1"
+    const val okHttp = "4.6.0"
+
+    // AndroidX
+    const val appCompat = "1.1.0"
+    const val recyclerView = "1.1.0"
+    const val navigation = "2.2.2"
+    const val fragment = "1.2.4"
+    const val test = "1.2.0"
+    const val espresso = "3.2.0"
+    const val constraintLayout = "2.0.0-beta5"
+    const val coreKtx = "1.3.0"
+    const val lifecycle = "2.2.0"
+
+}
