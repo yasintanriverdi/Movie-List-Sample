4b02e42d1bc22d07a245c89ae0e7cc7ba92d4c81
yasintanriverdi <yasin.tanriverdi@zenjob.com>
2020-05-22 16:09:22 +0200

    Added menu item to switch night mode

--- app/src/main/java/com/yasintanriverdi/movielistsample/MainActivity.kt
+++ app/src/main/java/com/yasintanriverdi/movielistsample/MainActivity.kt
@@ -2,8 +2,10 @@ package com.yasintanriverdi.movielistsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.yasintanriverdi.commons.extensions.isDarkTheme
 import com.yasintanriverdi.movielistsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +18,16 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         binding.toolbar.setupWithNavController(findNavController(R.id.nav_host_fragment))
+
+        binding.toolbar.setOnMenuItemClickListener {
+            val nightMode =
+                if (isDarkTheme()) {
+                    AppCompatDelegate.MODE_NIGHT_NO
+                } else {
+                    AppCompatDelegate.MODE_NIGHT_YES
+                }
+            AppCompatDelegate.setDefaultNightMode(nightMode)
+            true
+        }
     }
 }
---
+++ lib/commons/src/main/java/com/yasintanriverdi/commons/extensions/Context.kt
@@ -0,0 +1,7 @@
+package com.yasintanriverdi.commons.extensions
+
+import android.content.Context
+import android.content.res.Configuration
+
+fun Context.isDarkTheme() = resources.configuration.uiMode and
+        Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
